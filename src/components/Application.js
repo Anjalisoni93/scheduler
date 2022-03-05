c969485e671d867d80da0f0bc053dd923fac9508
Anjali Soni <anjalipatadiya@gmail.com>
2022-03-04 19:23:29 -0700

    Add useEffect and axios to request days and remover days array(hard coded)

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
+import axios from "axios";
 
-const days = [
+/* const days = [
   {
     id: 1,
     name: "Monday",
@@ -19,10 +21,73 @@ const days = [
     name: "Wednesday",
     spots: 0,
   },
-];
+]; */
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then(response => {
+      console.log(response);
+      setDays(response.data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+
+  const schedule = Object.values(appointments).map(appointment => {
+    return(
+      <Appointment 
+        key={appointment.id} 
+        id={appointment.id} 
+        time={appointment.time} 
+        interview={appointment.interview} 
+      />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,7 +111,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
 
     </main>
