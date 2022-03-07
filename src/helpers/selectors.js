b6bb6a6f30e08b17a8958cd8ebc8a8788a308321
Anjali Soni <anjalipatadiya@gmail.com>
2022-03-06 18:28:01 -0700

    Add a function for getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,23 @@
 export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(currentDay => currentDay.name === day);
-  console.log(filteredDays);
   if (filteredDays.length === 0) {
     return [];
   }
   else {
      return filteredDays[0].appointments.map((id) => state.appointments[id])
   }
+}
+
+export function getInterview (state, interview) {
+  if (interview) {
+    let idInterviewer = interview.interviewer;
+    let student = interview.student;
+
+    return {
+      student,
+      interviewer: state.interviewers[idInterviewer]
+    }
+  }
+
+  return null;
 }
